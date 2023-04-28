3ceefcfaa27ae39a356b78c997f7e46d892225b2
Lowrance, Peter <PLowrance@veritegroup.com>
2023-04-28 18:00:01 -0400

    Test ads

--- react-website/src/App.tsx
+++ react-website/src/App.tsx
@@ -72,6 +72,23 @@ function App() {
                 <GameStartPanel handleStart={handleStart} loading={gameStartRes.isFetching} error={gameStartRes.isError} />
               </EuiFlexItem>
             }
+            {gameState === 'NOT_STARTED' && gameStartRes.isFetching &&
+              <EuiFlexItem grow={false} style={{ width: '100%', maxWidth: 1000, marginLeft: 'auto', marginRight: 'auto' }}>
+                <EuiPanel hasBorder className='info-panel-apper'>
+                  <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5547157842419484"
+                    crossOrigin="anonymous"></script>
+                  <ins className="adsbygoogle"
+                    style={{display: 'block'}}
+                    data-ad-client="ca-pub-5547157842419484"
+                    data-ad-slot="7660836667"
+                    data-ad-format="auto"
+                    data-full-width-responsive="true"></ins>
+                  <script>
+                    (adsbygoogle = window.adsbygoogle || []).push({ });
+                  </script>
+                </EuiPanel>
+              </EuiFlexItem>
+            }
             {/* On mobile view, status goes here */}
             <EuiShowFor sizes={['xs', 's']}>
               {gameState === 'CHOOSING_ITEMS' || gameState === 'FACING_SCENARIOS' &&
